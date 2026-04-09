99b3bad6d207aa7d6e5747c7b865baa3e5bf3b0d
LucaTabbia <luca.tabbia97@gmail.com>
2026-04-09 16:02:31 +0900

    refactor(transaction_cubit): replace print with debugPrint for improved logging and ensure state reset only if not closed

--- lib/screens/mainScreen/tabs/shop/transactionCubit/transaction_cubit.dart
+++ lib/screens/mainScreen/tabs/shop/transactionCubit/transaction_cubit.dart
@@ -1,5 +1,5 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:flutter/foundation.dart';
 
 import '../../../../../models/request/transaction_request.dart';
 import '../../../../../repository/services/graphql/graphql.dart';
@@ -15,11 +15,15 @@ class TransactionCubit extends Cubit<TransactionState> {
     try {
       await graphQl.doTransaction(input);
       emit(TransactionDone());
-      Future.delayed(Duration(seconds: 4), () => {emit(TransactionInitial())});
+      Future.delayed(Duration(seconds: 4), () {
+        if (!isClosed) emit(TransactionInitial());
+      });
     } catch (e) {
-      print(e.toString());
+      debugPrint(e.toString());
       emit(TransactionError());
-      Future.delayed(Duration(seconds: 4), () => {emit(TransactionInitial())});
+      Future.delayed(Duration(seconds: 4), () {
+        if (!isClosed) emit(TransactionInitial());
+      });
     }
   }
 }
